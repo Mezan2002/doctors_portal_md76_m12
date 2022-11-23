55b1b53594c164c625a940a5e85bcc5e8517f765
Mezan2002 <mezanurr009@gmail.com>
2022-11-23 13:21:13 +0600

    Steps written

--- src/Pages/Dashboard/Payment/CheckOutForm/CheckOutForm.js
+++ src/Pages/Dashboard/Payment/CheckOutForm/CheckOutForm.js
@@ -1,13 +1,36 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
-import React from "react";
+import React, { useState } from "react";
 
 const CheckOutForm = () => {
+  const [cardError, setCardError] = useState("");
   const stripe = useStripe();
   const elements = useElements();
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
+    if (!stripe || !elements) {
+      return;
+    }
+    const card = elements.getElement(CardElement);
+    if (card === null) {
+      return;
+    }
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    if (error) {
+      console.log(error);
+      setCardError(error.message);
+    } else if (paymentMethod) {
+      console.log(paymentMethod);
+    } else {
+      setCardError("");
+    }
   };
+
   return (
     <form onSubmit={handleSubmit}>
       <CardElement
@@ -26,9 +49,16 @@ const CheckOutForm = () => {
           },
         }}
       />
-      <button type="submit" disabled={!stripe}>
+      <button
+        type="submit"
+        className="btn btn-primary mt-10 btn-block text-white"
+        disabled={!stripe}
+      >
         Pay
       </button>
+      {cardError && (
+        <p className="text-center text-red-500 mt-5">{cardError}</p>
+      )}
     </form>
   );
 };
--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -1,5 +1,10 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import CheckOutForm from "./CheckOutForm/CheckOutForm";
+
+const stripePromise = loadStripe(process.env.REACT_APP_stripe_PK);
 
 const Payment = () => {
   const bookingInfo = useLoaderData();
@@ -13,6 +18,12 @@ const Payment = () => {
           Please pay <strong>{price}$</strong>, your appintment on{" "}
           {appointmentDate} at {timeOfAppointment}
         </p>
+      </div>
+      <div className="border border-black p-10 w-7/12 rounded-2xl ">
+        <h3 className="text-3xl text-center mb-10">Payment Card</h3>
+        <Elements stripe={stripePromise}>
+          <CheckOutForm />
+        </Elements>
       </div>
     </div>
   );
